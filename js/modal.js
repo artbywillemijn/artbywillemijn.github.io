5008a332ec79a8f95c3f261014542af6523bfcb5
Lee Johnson <lee@givengain.ch>
2018-08-30 18:35:49 +0200

    add modal for images
    todo: break up every 4 images, add alt text

---
+++ js/modal.js
@@ -0,0 +1,18 @@
+// Get the modal
+var modal       = document.getElementById('modal-container');
+var modalImg    = document.getElementById("modal_img");
+var captionText = document.getElementById("caption");
+
+function showModal( src,alt ) {
+    modal.style.display = "block";
+    modalImg.src = src;
+    captionText.innerHTML = alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
